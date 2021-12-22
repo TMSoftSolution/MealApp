db06226163a381542adc49096b3454eef537dce6
tmcsoftdev <lovetallman426@gmail.com>
2021-12-22 00:41:42 -0800

    complete meal detail page

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
@@ -27,6 +25,8 @@ class MyApp extends StatelessWidget {
               fontFamily: 'RobotoCondensed',
               fontWeight: FontWeight.bold,
             )),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
       ),
       // home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,18 +1,86 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
+import '../models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
+
+  Widget buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.subtitle1,
+      ),
+    );
+  }
+
+  Widget buildContainer(Widget child) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10)),
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        height: 100,
+        width: 300,
+        child: child);
+  }
 
   @override
   Widget build(BuildContext context) {
     final arg =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final String id = arg['id'];
+    Meal selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == id);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Meal Detail'),
-      ),
-      body: Text('Meal Detail $id'),
-    );
+        appBar: AppBar(
+          title: Text(selectedMeal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  selectedMeal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              buildSectionTitle(context, 'Indegrients'),
+              buildContainer(
+                ListView.builder(
+                  itemBuilder: (ctx, index) => Card(
+                    color: Theme.of(ctx).colorScheme.secondary,
+                    child: Padding(
+                      child: Text(selectedMeal.ingredients[index]),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    ),
+                  ),
+                  itemCount: selectedMeal.ingredients.length,
+                ),
+              ),
+              buildSectionTitle(context, 'Steps'),
+              buildContainer(ListView.builder(
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                        leading: CircleAvatar(
+                          child: Text('# ${(index + 1)}'),
+                        ),
+                        title: Text(selectedMeal.steps[index])),
+                    Divider()
+                  ],
+                ),
+                itemCount: selectedMeal.steps.length,
+              )),
+            ],
+          ),
+        ));
   }
 }
